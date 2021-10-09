bbcd5c1ebc446fb0aba4a3b5914bf5e718ba6310
porridgewithraisins <santhanambr2002@gmail.com>
2021-10-09 19:36:03 +0530

    update developer scripts

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,2 +1,10 @@
-NODE_ENV=production node --inspect bot.js
+if [ $# -eq 0 ]; then
+    mode="development"
+else 
+    mode="$1"
+fi
+
+echo "Running node application in $mode mode"
+
+NODE_ENV=$mode node --inspect bot.js
 printf "\nYou can now visit chrome://inspect in a chromium-based browser to attach devtools to the application"
